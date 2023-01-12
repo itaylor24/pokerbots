ebb9f85a6c451e2ad3615083db3b96e12b10c4fa
jokorie <jokorie@mit.edu>
2023-01-12 10:34:09 -0500

    "all-in pair-hunting" vs "always-check" bot These are some things I've noticed: 1. With the current directory setup after this commit. If you want to make another bot you'll have to make a copy of the player file as we already know. importantly, you'll have to go into the commands.json file and input the name of the file you would like the entire "python_skeleton" directory (or directories on that level) to run. In other words if you would like to run 2 seperate bots you'll need two seperate directories, hence why I made the "python_poker_ai" directory 2. When you are ready to run your bot against another bot (or maybe itself) you should go into the "config.py" file and specify the file path of the "python_skeleton"-level directories for player A and one for player B 3. When you want to see the in-game analysis, check the gamelog.txt file to watch the play by play narration of the rounds. To see the overall final score, scroll to the bottom. It should be some 2000 lines deep 4. Lastly if you embedded any print statements into the player A or player B bot it will be stored in their respective "A.txt" or "B.txt" files 5. As we saw in the first lecture, even the simple "all-in pair-hunting " bot is set to beat the always-check bot
    Next Step:
    We want to build bots leagues better than these two existing bots. Consider:
    - Using board information and not just hole cards information
    - Search for more than just pairs
    - Using the montecarlo simulation to calculate probabilities

--- python_poker_ai/player.py
+++ python_poker_ai/player.py
@@ -16,37 +16,43 @@ class Player(Bot):
     def __init__(self):
         '''
         Called when a new game starts. Called exactly once.
+
         Arguments:
         Nothing.
+
         Returns:
         Nothing.
         '''
-        pass
+        self.strong_hole = False
 
     def handle_new_round(self, game_state, round_state, active):
         '''
         Called when a new round starts. Called NUM_ROUNDS times.
+
         Arguments:
         game_state: the GameState object.
         round_state: the RoundState object.
         active: your player's index.
+
         Returns:
         Nothing.
         '''
         #my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
         #game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
         #round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
-        #my_cards = round_state.hands[active]  # your cards
-        #big_blind = bool(active)  # True if you are the big blind
+        # my_cards = round_state.hands[active]  # your cards
+        #big_blind = bool(active)  # True if you are the big blind        
         pass
 
     def handle_round_over(self, game_state, terminal_state, active):
         '''
         Called when a round ends. Called NUM_ROUNDS times.
+
         Arguments:
         game_state: the GameState object.
         terminal_state: the TerminalState object.
         active: your player's index.
+
         Returns:
         Nothing.
         '''
@@ -55,39 +61,104 @@ class Player(Bot):
         #street = previous_state.street  # int of street representing when this round ended
         #my_cards = previous_state.hands[active]  # your cards
         #opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
+        self.strong_hole = False
         pass
 
     def get_action(self, game_state, round_state, active):
         '''
         Where the magic happens - your code should implement this function.
         Called any time the engine needs an action from your bot.
+
         Arguments:
         game_state: the GameState object.
         round_state: the RoundState object.
         active: your player's index.
+
         Returns:
         Your action.
         '''
         legal_actions = round_state.legal_actions()  # the actions you are allowed to take
-        #street = round_state.street  # int representing pre-flop, flop, turn, or river respectively
-        #my_cards = round_state.hands[active]  # your cards
-        #board_cards = round_state.deck[:street]  # the board cards
-        #my_pip = round_state.pips[active]  # the number of chips you have contributed to the pot this round of betting
-        #opp_pip = round_state.pips[1-active]  # the number of chips your opponent has contributed to the pot this round of betting
-        #my_stack = round_state.stacks[active]  # the number of chips you have remaining
-        #opp_stack = round_state.stacks[1-active]  # the number of chips your opponent has remaining
-        #continue_cost = opp_pip - my_pip  # the number of chips needed to stay in the pot
-        #my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
-        #opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
-        #if RaiseAction in legal_actions:
+        street = round_state.street  # int representing pre-flop, flop, turn, or river respectively
+        my_cards = round_state.hands[active]  # your cards
+        board_cards = round_state.deck[:street]  # the board cards
+        my_pip = round_state.pips[active]  # the number of chips you have contributed to the pot this round of betting
+        opp_pip = round_state.pips[1-active]  # the number of chips your opponent has contributed to the pot this round of betting
+        my_stack = round_state.stacks[active]  # the number of chips you have remaining
+        opp_stack = round_state.stacks[1-active]  # the number of chips your opponent has remaining
+        continue_cost = opp_pip - my_pip  # the number of chips needed to stay in the pot
+        my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
+        opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
+        # if RaiseAction in legal_actions:
         #    min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
         #    min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
         #    max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
-        if CheckAction in legal_actions:  # check-call
-            return CheckAction()
-        return CallAction()
+
+        self.allocate_cards(my_cards)
+
+
+        net_upper_raise_bound = round_state.raise_bounds()
+        stacks = [my_stack, opp_stack]
+
+        net_cost = 0
+        my_action = None
+
+        if (RaiseAction in legal_actions and self.strong_hole):
+            min_raise, max_raise = round_state.raise_bounds()
+            max_cost = max_raise - my_pip
+
+            if max_cost <= my_stack - net_cost:
+                my_action = RaiseAction(max_raise)
+                net_cost += max_cost
+
+            elif CallAction in legal_actions:
+                my_action = CallAction()
+                net_cost += continue_cost
+            
+            else:
+                my_action = CheckAction()
+        elif CheckAction in legal_actions:
+            my_action = CheckAction()
+
+        else:
+            my_action = CallAction()
+            net_cost += continue_cost
+
+        return my_action
+
+
+
+        # if CheckAction in legal_actions:  # check-call
+        #     return CheckAction()
+        # return CallAction()
+    
+    def allocate_cards(self, my_cards):
+        ranks = {} # number:suite
+
+        for card in my_cards: #for card in hand
+            card_rank = card[0]
+            card_suite = card[1]
+
+            if card_rank in ranks:
+                ranks[card_rank].append(card_suite)
+            else:
+                ranks[card_rank] = [card_suite]
+        
+        singles = []
+        pairs = []
+
+        for rank in ranks:
+            if len(ranks[rank]) == 1:
+                singles.append(rank)
+            else:
+                pairs.append(rank)
+        
+        if len(pairs) > 0:
+            self.strong_hole = True
+
+
+
+        
 
 
 if __name__ == '__main__':
     run_bot(Player(), parse_args())
-    
--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -16,10 +16,8 @@ class Player(Bot):
     def __init__(self):
         '''
         Called when a new game starts. Called exactly once.
-
         Arguments:
         Nothing.
-
         Returns:
         Nothing.
         '''
@@ -28,12 +26,10 @@ class Player(Bot):
     def handle_new_round(self, game_state, round_state, active):
         '''
         Called when a new round starts. Called NUM_ROUNDS times.
-
         Arguments:
         game_state: the GameState object.
         round_state: the RoundState object.
         active: your player's index.
-
         Returns:
         Nothing.
         '''
@@ -47,12 +43,10 @@ class Player(Bot):
     def handle_round_over(self, game_state, terminal_state, active):
         '''
         Called when a round ends. Called NUM_ROUNDS times.
-
         Arguments:
         game_state: the GameState object.
         terminal_state: the TerminalState object.
         active: your player's index.
-
         Returns:
         Nothing.
         '''
@@ -67,12 +61,10 @@ class Player(Bot):
         '''
         Where the magic happens - your code should implement this function.
         Called any time the engine needs an action from your bot.
-
         Arguments:
         game_state: the GameState object.
         round_state: the RoundState object.
         active: your player's index.
-
         Returns:
         Your action.
         '''
@@ -98,3 +90,4 @@ class Player(Bot):
 
 if __name__ == '__main__':
     run_bot(Player(), parse_args())
+    
